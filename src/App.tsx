0cea3bf6e6f1be121afab722f0d39d4939321aec
22209634 <22209634@students.latrobe.edu.au>
2025-09-07 09:45:54 +0400

    Add TelescopeView component with initial implementation and styling. Add telescope assets and move assets from src/ to public/.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Topbar from "@/components/Topbar/Topbar.tsx"
+import TelescopeView from "@/components/TelescopeView/TelescopeView.tsx"
 
 function App() {
   return (
     <main>
+        <TelescopeView />
         <Topbar />
     </main>
   );
---
+++ src/components/TelescopeView/TelescopeView.tsx
@@ -0,0 +1,10 @@
+import './TelescopeView.css';
+
+export default function TelescopeView() {
+    return (
+        <section className="telescope-view">
+            {/* Placeholder Image */}
+            <img alt="Starry Sky zoomed in" src="~/../public/assets/unsplash-telescope.jpg" />
+        </section>
+    )
+}
