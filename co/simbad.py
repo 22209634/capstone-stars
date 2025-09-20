250eaf3628b4ff8f9e536aab6f59bd82e6eb3d95
meracodeee <meeran.sbr@gmail.com>
2025-09-20 23:16:03 +1000

    Update simbad.py

--- co/simbad.py
+++ co/simbad.py
@@ -1,3 +1,12 @@
 from astroquery.simbad import Simbad
+from astropy.coordinates import Skycoord, EarthLocation, AltAz
+from astropy.time import Time
+import astropy.units as u
+
+lat, lon = -37.7, 145.05
+location = EarthLocation(lat = lat*u.deg, lon = lon*u.deg)
+time = Time.now()
+
+
 result = Simbad.query_object("M42")
 print(result)
