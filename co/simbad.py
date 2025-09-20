bcf13b3fb56cb8de3aa9db2f8920da7954a69ef5
meracodeee <meeran.sbr@gmail.com>
2025-09-20 23:58:18 +1000

    Update simbad.py

--- co/simbad.py
+++ co/simbad.py
@@ -2,10 +2,14 @@ from astroquery.simbad import Simbad
 from astropy.coordinates import Skycoord, EarthLocation, AltAz
 from astropy.time import Time
 import astropy.units as u
+import numpy as np
 
 lat, lon = -37.7, 145.05
 location = EarthLocation(lat = lat*u.deg, lon = lon*u.deg)
 time = Time.now()
+
+altaz_frame = Altaz(obstime = time, location = location)
+center = SkyCoord("
 
 
 result = Simbad.query_object("M42")
