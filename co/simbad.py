e9676a3b236bd37254c6f41b74370c8cfc1b9890
meracodeee <meeran.sbr@gmail.com>
2025-09-21 20:02:25 +1000

    Update simbad.py

--- co/simbad.py
+++ co/simbad.py
@@ -73,5 +73,9 @@ if __name__ == "__main__":
     print("No objects found. Check if night time or adjust filters.")
   else: 
     print(f"Visible objects (min_alt_deg = {min_alt_deg},, V <= {magnitude}): {len(objects)}\n")
+    print("Preview")
+    for obj in objects[:5]:
+      print(f" {obj["name"]} | alt = {obj["alt"]:.1f}
+
 
 
