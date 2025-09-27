ef1a74a9750627aa20d422e3bf58db596ab35271
meracodeee <meeran.sbr@gmail.com>
2025-09-27 20:08:23 +1000

    Update simbad.py

--- co/simbad.py
+++ co/simbad.py
@@ -41,8 +41,9 @@ def visible_objects_bundoora(min_alt_deg, magnitude):
   aa = coords.transform_to(altaz)
   mag = result["V"] if "V" in result.colnames else np.array([np.nan]*len(result))
   otype = result["OTYPE"] if "OTYPE" in result.colnames else np.array(["?"]*len(result))
-  names = result["MAIN_ID"]
-  if "IDS" in result.colnames:
+  if "MAIN_ID" in result.colnames:
+    names = result["MAIN_ID"]
+  elif "IDS" in result.colnames:
     names = result["IDS"]
   else:
     names = np.array([f"Obj_{i+1}" for i in range(len(result))])
