8f46c54b646d3fe86ca9da1686a14e66987e4de2
22209634 <22209634@students.latrobe.edu.au>
2025-10-08 21:28:03 +0400

    Remove old All-sky image and replace with a 2MASS sky survey image

--- src/components/AllSkyView/AllSkyView.tsx
+++ src/components/AllSkyView/AllSkyView.tsx
@@ -1,10 +1,25 @@
 import './AllSkyView.css';
 
 export default function AllSkyView() {
+    // Using CDS HiPS2FITS service to generate a wide-field sky image
+    // Using 2MASS survey which is smoother and doesn't have visible tile boundaries
+    const allSkyImageUrl = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?' +
+        'hips=CDS/P/2MASS/color&' +
+        'width=600&' +
+        'height=400&' +
+        'projection=TAN&' +
+        'coordsys=icrs&' +
+        'ra=0&' +
+        'dec=0&' +
+        'fov=60&' +
+        'format=png';
+
     return (
         <div className="allsky-view__wrapper">
-            {/* Placeholder Image */}
-            <img alt="Starry Sky" src="/assets/unsplash-all-sky.jpg" />
+            <img
+                alt="Wide-Field Sky View from 2MASS Survey"
+                src={allSkyImageUrl}
+            />
         </div>
     )
 }
