7b311fc3507a1189ee1757cc725d55c81462acfd
22209634 <22209634@students.latrobe.edu.au>
2025-10-08 21:34:04 +0400

    Add new All-sky-view image, generated from 2MASS sky survey

--- src/components/AllSkyView/AllSkyView.tsx
+++ src/components/AllSkyView/AllSkyView.tsx
@@ -4,15 +4,15 @@ export default function AllSkyView() {
     // Using CDS HiPS2FITS service to generate a wide-field sky image
     // Using 2MASS survey which is smoother and doesn't have visible tile boundaries
     const allSkyImageUrl = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?' +
-        'hips=CDS/P/2MASS/color&' +
-        'width=600&' +
-        'height=400&' +
-        'projection=TAN&' +
-        'coordsys=icrs&' +
-        'ra=0&' +
-        'dec=0&' +
-        'fov=60&' +
-        'format=png';
+    'hips=CDS/P/2MASS/color&' +
+    'width=600&' +
+    'height=400&' +
+    'projection=TAN&' +
+    'coordsys=icrs&' +
+    'ra=0&' +
+    'dec=0&' +
+    'fov=60&' +
+    'format=png';
 
     return (
         <div className="allsky-view__wrapper">
